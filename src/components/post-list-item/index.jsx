4d69e5e371b800bcd5d5c862c2a07f6a973157b8
Vadim Barkovski <VBarkovski@gomel.iba.by>
2021-04-07 11:37:47 +0300

    Added new methods

--- src/components/post-list-item/index.jsx
+++ src/components/post-list-item/index.jsx
@@ -1,23 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './style.css';
 
-const PostItem = () => {
-  return (
-    <li className='app-list-item d-flex justify-content-between'>
-      <span className='app-list-item-label'>
-        Hello World
-      </span>
-      <div className='d-flex justify-content-center align-items-center'>
-        <button type='button' className='btn-star btn-sm'>
-          <i className='fa fa-star'></i>
-        </button>
-        <button type='button' className='btn-trash btn-sm'>
-          <i className='fa fa-trash-o'></i>
-        </button>
-        <i className='fa fa-heart'></i>
+class PostItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      important: false,
+      like: false
+    };
+    this.onImportant = this.onImportant.bind(this);
+    this.onLike = this.onLike.bind(this);
+  }
+
+  onImportant() {
+    this.setState(({ important }) => ({
+      important: !important,
+    }))
+  }
+
+  onLike() {
+    this.setState(({ like }) => ({
+      like: !like,
+    }))
+  }
+
+  render() {
+    const { label } = this.props;
+    const { important = false, like = false } = this.state;
+
+    let classNames = 'app-list-item d-flex justify-content-between';
+    if (important) classNames += ' important';
+    if (like) classNames += ' like';
+
+    return (
+      <div className={classNames}>
+        <span className='app-list-item-label'>
+          {label}
+        </span>
+        <div className='d-flex justify-content-center align-items-center'>
+          <button type='button' className='btn-star btn-sm' onClick={this.onImportant}>
+            <i className='fa fa-star'></i>
+          </button>
+          <button type='button' className='btn-like btn-sm' onClick={this.onLike}>
+            <i className='fa fa-heart'></i>
+          </button>
+          <button type='button' className='btn-trash btn-sm'>
+            <i className='fa fa-trash-o'></i>
+          </button>
+        </div>
       </div>
-    </li>
-  )
+    )
+  }
 }
 
 export default PostItem;
