4549f42fcd09ef56370c50bfe042558bb56c9fff
Vadim Barkovski <VBarkovski@gomel.iba.by>
2021-04-07 13:24:07 +0300

    Added onDelete method

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Header, SearchPanel, PostFilter, PostList, PostAddForm } from '../';
 import './style.css';
 
-const App = () => {
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: [
+        { id: 1, label: 'Hello World', important: false },
+        { id: 2, label: 'Some Text', important: false },
+        { id: 3, label: 'New Text', important: false },
+        { id: 4, label: 'Some New Text', important: false },
+        { id: 5, label: 'And New Text', important: false },
+      ],
+    };
+    this.onDeletePost = this.onDeletePost.bind(this);
+  }
 
-  const data = [
-    {id: 1, label: 'Hello World', important: false },
-    {id: 2, label: 'Some Text', important: false },
-    {id: 3, label: 'New Text', important: true },
-  ]
+  onDeletePost = id => {
+    this.setState(({ data }) => {
+      const i = data.findIndex(elem => elem.id === id);
+      const arr = [...data];
+      arr.splice(i, 1);
+      return {
+        data: [...arr]
+      }
+    })
+  }
 
+  render() {
   return (
     <div className='app'>
       <Header />
@@ -17,10 +36,11 @@ const App = () => {
         <SearchPanel />
         <PostFilter />
       </div >
-      <PostList posts={data} />
+      <PostList
+        posts={this.state.data}
+        onDelete={this.onDeletePost} />
       <PostAddForm />
     </div>
   )
 }
-
-export default App;
+}
