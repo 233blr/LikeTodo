f3ae9785b47240a676080c98fbffdb5fa36df4ac
Vadim Barkovski <VBarkovski@gomel.iba.by>
2021-04-07 13:24:52 +0300

    Added button component

--- src/components/post-status-filter/index.jsx
+++ src/components/post-status-filter/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Button } from 'reactstrap';
 
 const PostFilter = () => {
   return (
     <div className='btn-group'>
-      <button type='button' className='btn btn-info'>Все</button>
-      <button type='button' className='btn btn-outline-secondary'>Понравилось</button>
+      <Button outline color='primary'>Все</Button>
+      <Button outline color="info">Понравилось</Button>
     </div>
   )
 }
