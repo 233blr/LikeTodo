90334473f7e46ae978469960db335d0f01db0252
Vadim Barkovski <VBarkovski@gomel.iba.by>
2021-04-07 13:24:37 +0300

    Fixed code

--- src/components/search-panel/index.jsx
+++ src/components/search-panel/index.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import './style.css';
 
-const SearchPanel = () => {
-  return <input className='form-control search-inp' type='text' placeholder='Поиск' />
-}
+const SearchPanel = () => (<input className='form-control search-input' type='text' placeholder='Поиск' />)
 
 export default SearchPanel;
