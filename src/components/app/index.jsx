60ab71eed1ca4052059ab1cfc4df7289f7ed2860
Vadim Barkovski <VBarkovski@gomel.iba.by>
2021-04-09 16:11:05 +0300

    Added onToggleImportant and onToggleICompleted

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -8,15 +8,19 @@ export default class App extends Component {
     super(props)
     this.state = {
       data: [
-        { id: 1, label: 'Hello World', important: false },
-        { id: 2, label: 'Some Text', important: false },
-        { id: 3, label: 'New Text', important: false },
-        { id: 4, label: 'Some New Text', important: false },
-        { id: 5, label: 'And New Text', important: false },
+        { id: 1, title: 'Hello World', important: false, completed: false },
+        { id: 2, title: 'Hello World', important: true, completed: false },
+        { id: 3, title: 'Hello World', important: false, completed: true },
+        { id: 4, title: 'Hello World', important: false, completed: false },
+        { id: 5, title: 'Hello World', important: false, completed: false },
+        { id: 6, title: 'Hello World', important: false, completed: false },
+        { id: 7, title: 'Hello World', important: false, completed: false },
       ],
     };
     this.onDeletePost = this.onDeletePost.bind(this);
     this.addItem = this.addItem.bind(this);
+    this.onToggleImportant = this.onToggleImportant.bind(this);
+    this.onToggleCompleted = this.onToggleCompleted.bind(this);
   }
 
   onDeletePost = id => {
@@ -24,34 +28,46 @@ export default class App extends Component {
       const i = data.findIndex(elem => elem.id === id);
       const arr = [...data];
       arr.splice(i, 1);
-      return {
-        data: arr
-      }
+      return { data: arr };
     })
   }
 
-  addItem = (text) => {
-    const newPost = { id: uuidv4(), label: text, important: false };
+  addItem = text => {
+    this.setState(({ data }) => ({ data: [...data, { id: uuidv4(), title: text, important: false, completed: false }] }));
+  }
+
+  onToggleImportant = id => {
     this.setState(({ data }) => {
-      return {
-        data: [...data, newPost]
-      }
-    }
-    )
-    console.log(text)
+      const i = data.findIndex(elem => elem.id === id);
+      const arr = [...data];
+      const item = data[i];
+      arr.splice(i, 1, { ...item, important: !item.important });
+      return { data: arr };
+    });
+  }
+
+  onToggleCompleted = id => {
+    this.setState(({ data }) => {
+      const i = data.findIndex(elem => elem.id === id);
+      const arr = [...data];
+      const item = data[i];
+      arr.splice(i, 1, { ...item, completed: !item.completed });
+      return { data: arr };
+    })
   }
 
   render() {
   return (
     <div className='app'>
       <Header />
-      < div className='search-panel d-flex' >
+      <div className='search-panel d-flex'>
         <SearchPanel />
         <PostFilter />
       </div >
-      <PostList
-        posts={this.state.data}
-        onDelete={this.onDeletePost} />
+      <PostList posts={this.state.data}
+        onDelete={this.onDeletePost}
+        onToggleImportant={this.onToggleImportant}
+        onToggleCompleted={this.onToggleCompleted} />
       <PostAddForm onAdd={this.addItem} />
     </div>
   )
