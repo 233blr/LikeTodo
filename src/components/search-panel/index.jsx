b79a1753d7f92c6d897a12b22c1e9d581cd69259
Vadim Barkovski <VBarkovski@gomel.iba.by>
2021-04-09 19:24:00 +0300

    Added onCange event

--- src/components/search-panel/index.jsx
+++ src/components/search-panel/index.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import './style.css';
 
-const SearchPanel = () => (<input className='form-control search-input' type='text' placeholder='Search' />)
+const SearchPanel = ({ onAddTerm }) => {
+  return (
+    <input
+      className='form-control search-input'
+      type='text'
+      placeholder='Filter'
+      onChange={onAddTerm} />
+  )
+}
 
 export default SearchPanel;
