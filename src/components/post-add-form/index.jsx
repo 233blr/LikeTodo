4ac6b1f5b9a78f3143b97105ca567c9a332f757f
Vadim Barkovski <VBarkovski@gomel.iba.by>
2021-04-09 19:25:12 +0300

    Added new functions

--- src/components/post-add-form/index.jsx
+++ src/components/post-add-form/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button } from 'reactstrap';
 import styled from 'styled-components';
 
-const PostForm = styled.div`
+const PostForm = styled.form`
 display: flex;
 margin-top: 20px;
 input {
@@ -12,13 +12,40 @@ input {
 }
 `
 
-const PostAddForm = ({ onAdd }) => {
-  return (
-    <PostForm>
-      <input type='text' placeholder='Add task...' className='form-control' />
-      <Button type='submit' outline color="success" onClick={() => onAdd('hi')}>Add</Button>
+export default class PostAddForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: ''
+    }
+    this.onAddText = this.onAddText.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  onAddText(e) {
+    this.setState({
+      text: e.target.value
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    if (!this.state.text) {
+      alert('add text')
+      return
+    }
+    this.props.onAdd(this.state.text);
+    this.setState({
+      text: ''
+    });
+  }
+
+  render() {
+    return (
+      <PostForm onSubmit={this.onSubmit}>
+        <input type='text' placeholder='Add task...' className='form-control' onChange={this.onAddText} value={this.state.text} />
+        <Button type='submit' color="success">Add</Button>
     </PostForm>
   )
+  }
 }
-
-export default PostAddForm;
